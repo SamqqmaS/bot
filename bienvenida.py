50b53efd76815276371d61fbb8fa475f299b7ad1
Samq <162061475+SamqqmaS@users.noreply.github.com>
2025-01-06 14:08:27 -0500

    Create bienvenida.py

---
+++ bienvenida.py
@@ -0,0 +1,6 @@
+# bienvenido/bienvenida.py
+
+def generar_bienvenida(nombre_usuario):
+    # Crear el mensaje de bienvenida
+    bienvenida = f"¡Bienvenido/a al servidor, {nombre_usuario}! 🎉😄"
+    return bienvenida
